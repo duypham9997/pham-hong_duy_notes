32479bc1ae610e379e6f64c291d7bef13cb47ee3
duyphamhong <duyphamhong97@gmail.com>
2023-06-18 15:08:44 +0700

    Initial Commit

---
+++ Pham Hong Duy Notes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Pham Hong Duy Notes
+//
+//  Created by PHAM HONG DUY on 6/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pham Hong Duy Notes/Pham_Hong_Duy_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pham_Hong_Duy_NotesApp.swift
+//  Pham Hong Duy Notes
+//
+//  Created by PHAM HONG DUY on 6/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Pham_Hong_Duy_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
